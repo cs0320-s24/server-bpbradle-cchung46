11755a0d0dd861fd2ac0f91962b236453512afa9
Cecily Chung <cjchung@usc.edu>
2024-02-18 16:58:03 -0500

    chore: updated some files according to github

--- src/test/java/edu/brown/cs/student/main/ACSRepoTest.java
+++ src/test/java/edu/brown/cs/student/main/ACSRepoTest.java
@@ -1,4 +1,3 @@
 package edu.brown.cs.student.main;
 
-public class ACSRepoTest {
-}
+public class ACSRepoTest {}
--- src/test/java/edu/brown/cs/student/main/SearchCSVTest.java
+++ src/test/java/edu/brown/cs/student/main/SearchCSVTest.java
@@ -1,4 +1,3 @@
 package edu.brown.cs.student.main;
 
-public class SearchCSVTest {
-}
+public class SearchCSVTest {}
